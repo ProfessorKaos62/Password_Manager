a0e8576e3dd1f7f9586f6c56b4b0aa23080f2e4e
Jeremy Hines <107685330+ProfessorKaos62@users.noreply.github.com>
2023-06-26 07:16:18 -0400

    Canvas created and image added

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('test')
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('Password Manager')
+window.config(padx=20, pady=20, bg='white')
+
+canvas = Canvas(width=200, height=200, bg='white', highlightthickness=0)
+myimg = PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=myimg)
+canvas.grid(column=0, row=0)
+
+
+
+
+
+
+
+
+canvas.mainloop()
